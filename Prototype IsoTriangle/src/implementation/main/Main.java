526c314da5dc0c22d6dbc4c5b7aa8b8d8d5f2183
Pacôme <Pacme@pacome-pc-3>
2016-03-23 19:46:19 +0100

    Commit maison
    Ajout d'une classe contenant des fonctions statiques pour générer des
    niveaux. Il faudra en faire une factory plus propre (peut-être ranger
    les niveaux par collection de mots clefs)
    Ranger par mots clefs : difficulté 1/10, forme, nombre triangles...

--- Prototype IsoTriangle/src/implementation/main/Main.java
+++ Prototype IsoTriangle/src/implementation/main/Main.java
@@ -27,9 +27,12 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
 	public static void main(String[] args) {
+
+		
+		// -> wow !
+		System.gc();
 		
 		launch(args);
-		//launch(args);
 	}
 
 	@Override
@@ -44,7 +47,7 @@ public class Main extends Application {
 
 		catcher.addCatcher(PaintableEntity.class, new ObjectCatcher<PaintableEntity>(PaintableEntity.class));
 		catcher.addCatcher(GraphicalAsset.class, new ObjectCatcher<GraphicalAsset>(GraphicalAsset.class));
-		
+
 		IndexMaster index = world.getAnalyst().getIndexMaster();
 
 		index.addIndexer(Booster.class, new ObjectIndexer<Booster>(Booster.class));
@@ -58,83 +61,20 @@ public class Main extends Application {
 		world.getAnalyst().accessIndexers();
 		
 		JavaFXGameLoop game_loop = new JavaFXGameLoop(artist, world.getAnalyst().getIndexMaster());
-		
-		//MovingEntity entity1 = new Square(world.getAnalyst().getIndexMaster());
-		//MovingEntity entity2 = new Square(world.getAnalyst().getIndexMaster());
-		//MovingEntity entity3 = new Triangle();
-		
-		// -> wow !
-		System.gc();
-		
+
 		master.spawnEntity(new Graphic2DFloorCover(index));
 		
-		master.spawnEntity(new Floor(250, 250, 560, 560, 45));
-		master.spawnEntity(new Floor(500, 500, 200, 200, 45));
+		// Début des objets
 		
-		for(int x = 150; x < 650; x += 100)
-			for(int y = 150; y < 650; y += 100) {
-				Booster booster = new Booster();
-				booster.setX(x);
-				booster.setY(y);
-				master.spawnEntity(booster);
-			}
-		//master.spawnEntity( new SquareSpawner(master, world.getAnalyst().getIndexMaster(), 150, 150) );
+		TestLevels.level_virage_simple(master, index);
 		
-		Square.SquareFactory alpha_factory = new Square.SquareFactory(master, index, Square.alpha_family);
-		Square.SquareFactory beta_factory = new Square.SquareFactory(master, index, Square.beta_family);
-		//Square.SquareFactory gamma_factory = new Square.SquareFactory(master, index, Square.gamma_family);
-
-		master.spawnEntity( new Spawner(master, alpha_factory, 150, 150));
-		master.spawnEntity( new Spawner(master, beta_factory, 550, 550));
-		
-		master.spawnEntity( new Objective(master, index, 450, 550, 20, Square.alpha_family) );
-		master.spawnEntity( new Objective(master, index, 250, 150, 20, Square.beta_family) );
-		
-		master.spawnEntity( new CheckPoint(world.getAnalyst().getIndexMaster(), 150, 550, 20, Square.alpha_family, "cp1") );
-		master.spawnEntity( new CheckPoint(world.getAnalyst().getIndexMaster(), 550, 150, 20, Square.alpha_family, "cp2") );
-		/*world.addEntity(entity1);
-		world.addEntity(entity2);
-		//world.addEntity(entity3);
-		
-		entity1.setX(100);
-		entity1.setY(100);
-		entity1.setAngle(0);
-		entity1.notifyMove();
-		
-		entity2.setX(100);
-		entity2.setY(200);
-		entity2.addXAcceleration(1);
-		entity2.setAngle(0);
-		entity2.notifyMove();
-		
-		/*entity3.setX(100);
-		entity3.setY(300);
-		entity3.setAngle(1);
-		entity3.notifyMove();*/
+		// Fin des objets
 		
 		primary_stage.setTitle("IsoTriangle Prototype");
 		primary_stage.setScene(new Scene(artist.getRoot(), 1000, 1000));
 		primary_stage.show();
 		
 		game_loop.start();
-		
-		/*new Thread(new Runnable() {
-
-			@Override
-			public void run() {
-				for(;;) {
-					entity.setAngle( entity.getAngle() + 1);
-					if( entity.getX() < 500 )
-						entity.setX( entity.getX() + 1 );
-					else
-						entity.setX( entity.getX() - 1 );
-					entity.notifyMove();
-					try {
-						Thread.sleep(10);
-					} catch (InterruptedException e) {	e.printStackTrace(); }
-				}
-			}
-		}).start();*/
 	}
 
 }
---
+++ Prototype IsoTriangle/src/implementation/main/TestLevels.java
@@ -0,0 +1,52 @@
+package implementation.main;
+
+import graphics.twodimensions.object.Graphic2DFloorCover;
+import implementation.object.Booster;
+import implementation.object.CheckPoint;
+import implementation.object.Floor;
+import implementation.object.Objective;
+import implementation.object.Spawner;
+import implementation.object.Square;
+import implementation.world.IndexMaster;
+import core.world.WorldMaster;
+
+public abstract class TestLevels {
+	public static void level_diago_mauvais(WorldMaster master, IndexMaster index) {
+		master.spawnEntity(new Floor(250, 250, 560, 560, 45));
+		master.spawnEntity(new Floor(500, 500, 200, 200, 45));
+		
+		for(int x = 150; x < 650; x += 100)
+			for(int y = 150; y < 650; y += 100) {
+				Booster booster = new Booster();
+				booster.setX(x);
+				booster.setY(y);
+				master.spawnEntity(booster);
+			}
+		
+		Square.SquareFactory alpha_factory = new Square.SquareFactory(master, index, Square.alpha_family);
+		Square.SquareFactory beta_factory = new Square.SquareFactory(master, index, Square.beta_family);
+
+		master.spawnEntity( new Spawner(master, alpha_factory, 150, 150));
+		master.spawnEntity( new Spawner(master, beta_factory, 550, 550));
+		
+		master.spawnEntity( new Objective(master, index, 450, 550, 20, Square.alpha_family) );
+		master.spawnEntity( new Objective(master, index, 250, 150, 20, Square.beta_family) );
+		
+		master.spawnEntity( new CheckPoint(index, 150, 550, 20, Square.alpha_family, "cp1") );
+		master.spawnEntity( new CheckPoint(index, 550, 150, 20, Square.alpha_family, "cp2") );
+	}
+	
+	public static void level_virage_simple(WorldMaster master, IndexMaster index) {
+		master.spawnEntity(new Floor(525, 275, 846, 350, 0));
+		master.spawnEntity(new Floor(275, 525, 350, 850, 0));
+		
+		for(int x = 175; x < 950; x += 100)
+			for(int y = 175; y < 950; y += 100) {
+				if( x > 400 && y > 400 ) continue;
+				Booster booster = new Booster();
+				booster.setX(x);
+				booster.setY(y);
+				master.spawnEntity(booster);
+			}
+	}
+}
